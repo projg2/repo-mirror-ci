6d09200832bc81a78fe7b2d2f2de88d25f4ef092
Michał Górny <mgorny@gentoo.org>
2015-06-14 19:27:23 +0200

    Add HTML output tools

---
+++ summary2html.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import json
+import os.path
+
+with open('summary.json') as f:
+    repos = json.load(f)
+
+print('''
+<html>
+<head>
+    <meta charset='utf-8'/>
+    <link rel='stylesheet' type='text/css' href='repo-status.css'/>
+</head>
+<body>
+    <table>
+        <tr><th>Repository</th><th>Status</th></tr>
+''')
+
+status_mapping = {
+    'GOOD': 'all good!',
+    'BAD_CACHE': 'cache regen failed',
+    'INVALID_METADATA': 'invalid repository metadata (wrong masters= most likely)',
+    'EMPTY': 'repository empty',
+    'SYNC_FAIL': 'sync failed for repository',
+    'UNSUPPORTED': 'repository VCS unsupported',
+    'REMOVED': 'repository removed',
+}
+
+for r, s in repos.items():
+    if os.path.isfile('%s.log' % r):
+        r = '<a href="%s.log">%s</a>' % (r, r)
+    print('        <tr class="%s"><td>%s</td><td>%s</td>\n'
+            % (s, r, status_mapping[s]))
+
+print('''
+    </table>
+</body>
+</html>
+''')
