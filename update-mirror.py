fa75f46e0827983ab1f660dac369c894379d2299
Michał Górny <mgorny@gentoo.org>
2015-08-09 09:48:53 +0200

    Write repositories.xml for the mirror

--- update-mirror.py
+++ update-mirror.py
@@ -4,6 +4,7 @@ import json
 import os
 import os.path
 import sys
+import xml.etree.ElementTree as et
 
 import github
 
@@ -13,7 +14,29 @@ GITHUB_TOKEN_FILE = os.path.expanduser('~/.github-token')
 GITHUB_ORG = 'gentoo-mirror'
 
 
-def main(summary_path):
+def gh_sources(r):
+    return (
+        ('git', r.clone_url),
+        ('git', r.ssh_url),
+        ('git', r.git_url),
+        ('svn', r.svn_url + '/trunk'),
+    )
+
+
+# DTD expects a particular order
+DTD_ORDER = ['name', 'description', 'longdescription', 'homepage',
+        'owner', 'source', 'feed']
+
+
+def dtd_sort_key(av):
+    a, v = av
+    if a in DTD_ORDER:
+        return DTD_ORDER.index(a)
+    else:
+        return len(DTD_ORDER)
+
+
+def main(summary_path, repos_xml_path):
     with open(summary_path) as f:
         repos = json.load(f)
 
@@ -52,6 +75,7 @@ def main(summary_path):
             gh_repos.add(r.name)
             if repos[r.name]['x-can-update']:
                 to_update.append(r)
+            repos[r.name]['x-mirror-sources'] = gh_sources(r)
     sys.stderr.write('\n')
 
     # 1. delete stale repos
@@ -70,7 +94,61 @@ def main(summary_path):
                     homepage = data.get('homepage') or github.GithubObject.NotSet,
                     has_issues = False,
                     has_wiki = False)
+            repos[r]['x-mirror-sources'] = gh_sources(gr)
             to_update.append(gr)
+
+    # 3. write a new repositories.xml for them
+    root = et.Element('repositories')
+    root.set('version', '1.0')
+
+    for r, data in sorted(repos.items()):
+        if 'x-mirror-sources' not in data:
+            continue
+
+        rel = et.Element('repo')
+        for attr, val in sorted(data.items(), key=dtd_sort_key):
+            if attr.startswith('x-'):
+                continue
+            elif attr == 'source': # replace
+                for t, url in data['x-mirror-sources']:
+                    subel = et.Element('source')
+                    subel.set('type', t)
+                    subel.text = url
+                    rel.append(subel)
+            elif attr in ('quality', 'status'): # attributes
+                rel.set(attr, val)
+            elif attr in ('name', 'homepage'): # single-value
+                subel = et.Element(attr)
+                subel.text = val
+                rel.append(subel)
+            elif attr in ('description', 'longdescription'): # lang-dict
+                for l, v in val.items():
+                    subel = et.Element(attr)
+                    subel.set('lang', l)
+                    subel.text = v
+                    rel.append(subel)
+            elif attr in ('owner', 'feed'): # lists
+                for v in val:
+                    subel = et.Element(attr)
+                    if attr == 'owner':
+                        for k, subval in v.items():
+                            if k == 'type':
+                                subel.set(k, subval)
+                            else:
+                                subsubel = et.Element(k)
+                                subsubel.text = subval
+                                subel.append(subsubel)
+                    else:
+                        subel.text = v
+                    rel.append(subel)
+
+        root.append(rel)
+
+    xml = et.ElementTree(root)
+    with open(repos_xml_path, 'wb') as f:
+        f.write(b'<?xml version="1.0" encoding="UTF-8"?>\n')
+        f.write(b'<!DOCTYPE repositories SYSTEM "http://www.gentoo.org/dtd/repositories.dtd">\n')
+        xml.write(f, 'UTF-8', False)
 
     print('DELETED_REPOS = %s' % ' '.join(r.name for r in to_remove))
     print('REPOS = %s' % ' '.join(r.name for r in to_update))
