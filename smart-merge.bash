736cf697c519a87c442124ef0aee7dacb1aa47fd
Michał Górny <mgorny@gentoo.org>
2015-08-12 17:11:28 +0200

    mirrors: Enable smart merges of upstream git commits
    Smart merges try to gracefully merge upstream commits with mirrors and
    cache updates. We use 'ours' strategy to forcefully reset on upstream
    resets (force pushes, repo changes) while making it possible for mirror
    users to do conflict-free pulls.

---
+++ smart-merge.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e -x
+
+repo=${1}
+mirror=${2}
+branch=${3}
+
+[[ ${repo} && ${mirror} && ${branch} ]]
+
+# no git, no fun
+[[ -d ${repo}/.git ]] || exit 0
+
+cd "${mirror}"
+git fetch "${repo}" "${branch}:refs/orig/${branch}"
+if git merge-base "${branch}" "refs/orig/${branch}" > /dev/null; then
+	# regular update
+	git merge -q -s recursive -X theirs -m "Merge updates from ${branch}" "refs/orig/${branch}"
+else
+	# repo rewrite
+	git checkout -q "refs/orig/${branch}"
+	git merge -q -s ours -m "Merge/replace with the new version of ${branch} (reversed 'ours' strategy)" "${branch}"
+	git branch -f "${branch}" HEAD
+	git checkout "${branch}"
+fi
