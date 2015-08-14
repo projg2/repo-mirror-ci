3ed9b60076140cf8fef51acee621b77f69a26265
Michał Górny <mgorny@gentoo.org>
2015-08-14 23:12:08 +0200

    mirror: Handle conflicts in smart-merge

--- smart-merge.bash
+++ smart-merge.bash
@@ -14,7 +14,25 @@ cd "${mirror}"
 git fetch "${repo}" "${branch}:refs/orig/${branch}"
 if git merge-base "${branch}" "refs/orig/${branch}" > /dev/null; then
 	# regular update
-	git merge -q -s recursive -X theirs -m "Merge updates from ${branch}" "refs/orig/${branch}"
+	if ! git merge -q -s recursive -X theirs -m "Merge updates from ${branch}" "refs/orig/${branch}"; then
+		# check for conflicts
+		conflicts=no
+		while read st filename rest; do
+			case "${st}" in
+				DD|AU|UD|UA|DU|AA|UU)
+					# be lazy, handle all merge conflicts via rm...
+					git rm --cached "${filename}"
+					conflicts=yes
+					;;
+			esac
+		done < <(git status --porcelain --untracked-files=no)
+		if [[ ${conflicts} == yes ]]; then
+			git commit -q -m "Merge updates from ${branch}"
+		else
+			echo "** git merge failed, no conflicts found" >&2
+			exit 1
+		fi
+	fi
 elif ! git rev-parse HEAD &>/dev/null; then
 	# empty repo
 	git merge -q --ff "refs/orig/${branch}"
