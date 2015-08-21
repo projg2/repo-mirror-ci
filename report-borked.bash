eed88c240c2f3551de848a698ae94fd4e0439db9
Michał Górny <mgorny@gentoo.org>
2015-08-21 20:11:56 +0200

    report-borked: send e-mails to the ml

--- report-borked.bash
+++ report-borked.bash
@@ -5,12 +5,11 @@ set -e -x
 borked_list=/home/mgorny/gentoo-ci/borked.list
 borked_last=${borked_list%.list}.last
 uri_prefix=https://gentoo.github.io/gentoo-qa-results/
-mail_to=travis-ci@gentoo.org
-reply_to=mgorny@gentoo.org
+mail_to=gentoo-automated-testing@lists.gentoo.org
 
 if [[ ! -s ${borked_list} ]]; then
 	if [[ -s ${borked_last} ]]; then
-		subject="[gentoo-ci] FIXED: all failures have been fixed"
+		subject="FIXED: all failures have been fixed"
 		mail="
 Everything seems nice and cool now."
 	else
@@ -18,17 +17,17 @@ Everything seems nice and cool now."
 	fi
 else
 	if [[ ! -s ${borked_last} ]]; then
-		subject="[gentoo-ci] BROKEN: repository became broken!"
+		subject="BROKEN: repository became broken!"
 		mail="
 Looks like someone just broke Gentoo! Please take a look
 at the following pkgcheck failures:"
 	elif [[ $(wc -l <"${borked_list}") -gt $(wc -l <"${borked_last}") ]]; then
-		subject="[gentoo-ci] BROKEN: repository is even more broken!"
+		subject="BROKEN: repository is even more broken!"
 		mail="
 Looks like the breakage list has just grown! Please take a look
 at the following pkgcheck failures:"
 	elif [[ $(wc -l <"${borked_list}") -lt $(wc -l <"${borked_last}") ]]; then
-		subject="[gentoo-ci] BROKEN: repository is less broken now!"
+		subject="BROKEN: repository is less broken now!"
 		mail="
 Looks like the breakage list has just shrinked! Good work, but please
 fix the remaining pkgcheck failures:"
@@ -39,7 +38,6 @@ fi
 
 mail="Subject: ${subject}
 To: <${mail_to}>
-Reply-To: <${reply_to}>
 Content-Type: text/plain; charset=utf8
 ${mail}
 
