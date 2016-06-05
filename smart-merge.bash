bd8545f15536156f9dbf7b4c4a90649e4b130dba
Michał Górny <mgorny@gentoo.org>
2016-06-05 12:03:37 +0200

    smart-merge: Merge non-master default branch correctly

--- smart-merge.bash
+++ smart-merge.bash
@@ -3,12 +3,16 @@ set -e -x
 
 repo=${1}
 mirror=${2}
-branch=${3}
+m_branch=${3}
 
-[[ ${repo} && ${mirror} && ${branch} ]]
+[[ ${repo} && ${mirror} && ${m_branch} ]]
 
 # no git, no fun
 [[ -d ${repo}/.git ]] || exit 0
+
+cd "${repo}"
+branch=$(git symbolic-ref -q --short HEAD)
+[[ ${branch} ]] || exit 0
 
 cd "${mirror}"
 git fetch "${repo}" "+${branch}:refs/orig/${branch}"
@@ -39,7 +43,7 @@ elif ! git rev-parse HEAD &>/dev/null; then
 else
 	# repo rewrite
 	git checkout -q "refs/orig/${branch}"
-	git merge -q -s ours -m "Merge/replace with the new version of ${branch} (reversed 'ours' strategy)" "${branch}"
-	git branch -f "${branch}" HEAD
-	git checkout "${branch}"
+	git merge -q -s ours -m "Merge/replace with the new version of ${branch} (reversed 'ours' strategy)" "${m_branch}"
+	git branch -f "${m_branch}" HEAD
+	git checkout "${m_branch}"
 fi
