55f3d9142eb69e05ebb199470f4a87c7b2ee39dc
Michał Górny <mgorny@gentoo.org>
2016-11-08 16:24:04 +0100

    bisect: Attempt to fix detecting earlier breakages

--- bisect-borked.bash
+++ bisect-borked.bash
@@ -13,6 +13,6 @@ cd "${SYNC_DIR}/gentoo"
 initial_commit=$(git rev-parse HEAD)
 trap "git bisect reset; [[ \$(git rev-parse HEAD) == '${initial_commit}' ]] || git checkout -q '${initial_commit}'" EXIT
 
-git bisect start --no-checkout "${bad}" "${good}"
+git bisect start --no-checkout "${bad}" "${good}^"
 git bisect run "${SCRIPT_DIR}"/bisect-run-pkgcheck.bash "${pkgs[@]}"
 git rev-parse --short bisect/bad >&3
