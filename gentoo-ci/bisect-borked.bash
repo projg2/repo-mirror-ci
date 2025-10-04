aaecb88637daf80948b8337d2a046f883d0d36fa
Michał Górny <mgorny@gentoo.org>
2025-10-04 16:56:52 +0200

    Also git bisect, sigh
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- gentoo-ci/bisect-borked.bash
+++ gentoo-ci/bisect-borked.bash
@@ -20,6 +20,6 @@ cd -- "${SYNC_DIR}/gentoo"
 initial_commit=$(git rev-parse HEAD)
 trap "git bisect reset; [[ \$(git rev-parse HEAD) == '${initial_commit}' ]] || git checkout -q '${initial_commit}'" EXIT
 
-git bisect start --no-checkout -- "${bad}" "${good}^"
+git bisect start --no-checkout "${bad}" "${good}^"
 git bisect run "${SCRIPT_DIR}"/gentoo-ci/bisect-run-pkgcheck.bash "${flag}" "${pkgs[@]}"
 git rev-parse --short bisect/bad >&3
